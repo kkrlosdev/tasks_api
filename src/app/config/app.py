a2d7b65afece4883e3fb09588d0b755cf529eb95
Carlos <rocarlos1205@gmail.com>
2026-03-22 17:15:17 -0500

    feat: enhance health check endpoint and load environment variables

---
+++ src/app/config/app.py
@@ -0,0 +1,8 @@
+import datetime
+
+from .get_required_env import get_required_env
+
+SERVICE_NAME = get_required_env("SERVICE_NAME")
+ENVIRONMENT = get_required_env("ENVIRONMENT")
+STARTUP_TIME = datetime.datetime.now(datetime.UTC).isoformat()
+API_PORT = get_required_env("API_PORT")
