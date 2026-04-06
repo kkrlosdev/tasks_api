6eae44b3b832f504cd5bb83f8ada6aa1bca9aee9
Carlos <rocarlos1205@gmail.com>
2026-04-05 19:29:33 -0500

    feat: add SQL querys to the new schema of database with tags and criticities tables
    The new tasks model includes 2 new properties: criticities and tags. This allows us to enhance the concept of a task.
    Setup script now changed the name of the function responsible for database schema creation:
        - Now, the script is responsible for creating ALL database schema not only 'tasks' table,
          as it was the way it did before.
        - The function changed from create_tasks_table() to create_database_schema()
        - The operation for dropping tasks table was replaced by dropping all database, we could add it later but we maybe have
          to deal with other things. This is the current state.

--- src/db/scripts/setup.py
+++ src/db/scripts/setup.py
@@ -3,7 +3,7 @@ import click
 
 from db.connection import connect
 
-def create_tasks_table():
+def create_database_schema():
     try:
         conn = connect()
     except Exception as e:
@@ -12,20 +12,51 @@ def create_tasks_table():
     cursor = conn.cursor()
 
     try:
+        # Activar constraints de foreign keys ya que SQLite no las valida
+        # por defecto para ninguna operación.
+        cursor.execute("PRAGMA foreign_keys = ON;")
+
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS criticities(
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL UNIQUE
+                );
+                """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS tags(
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE
+            );
+            """)
+
         cursor.execute("""
                     CREATE TABLE IF NOT EXISTS tasks(
-                                        id INTEGER PRIMARY KEY,
-                                        name TEXT NOT NULL,
-                                        begin_date DATETIME,
-                                        end_date DATETIME,
-                                        short_description VARCHAR(100),
-                                        long_description TEXT,
-                                        status INT
-                    );
+                        id INTEGER PRIMARY KEY,
+                        name TEXT NOT NULL,
+                        begin_date DATETIME,
+                        end_date DATETIME,
+                        short_description VARCHAR(100),
+                        long_description TEXT,
+                        status INT,
+                        criticity_id INTEGER,
+                        FOREIGN KEY (criticity_id) REFERENCES criticities(id)
+                        );
                     """)
-        print("Table 'tasks' created successfully!")
+
+        cursor.execute("""
+                CREATE TABLE IF NOT EXISTS task_tags(
+                    task_id INTEGER,
+                    tag_id INTEGER,
+                    PRIMARY KEY (task_id, tag_id),
+                    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+                    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+                );
+                """)
+
+        print("Database schema created successfully!")
     except Exception as e:
-        raise Exception(f"Could not create table: {e}")
+        raise Exception(f"Could not create database: {e}")
     finally:
         cursor.close()
         conn.close()
@@ -48,7 +79,7 @@ def drop_table(table_name: str):
         conn.close()
 
 if __name__ == "__main__":
-    print("1. Create table 'tasks'\n2. Drop table 'tasks'\n3. Exit")
+    print("1. Create database schema\n2. Drop all database\n3. Exit")
     try:
         opt = int(input("$ "))
     except Exception as e:
@@ -57,9 +88,13 @@ if __name__ == "__main__":
 
     match opt:
         case 1:
-            create_tasks_table()
+            create_database_schema()
         case 2:
             condition = click.confirm("Are you completely sure? Do this on your OWN risk.")
-            if condition: drop_table("tasks")
+            if condition:
+                drop_table("criticities")
+                drop_table("tags")
+                drop_table("tasks")
+                drop_table("task_tags")
         case _:
             print("bye")
