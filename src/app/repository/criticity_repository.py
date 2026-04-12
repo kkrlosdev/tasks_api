242bc3b20f458c03ebe997803eea575637e3626a
Carlos <rocarlos1205@gmail.com>
2026-04-12 11:19:19 -0500

    feat(repository): added method to get criticity by id
    Also remove validation of rowcount when creating a criticity, the exception now reach a top-level in service and will be handled there.

--- src/app/repository/criticity_repository.py
+++ src/app/repository/criticity_repository.py
@@ -3,15 +3,20 @@ from typing import Any
 from app.repository.base_repository import BaseRepository, ExecuteMode
 
 class CriticityRepository(BaseRepository):
-    def create_criticity(self, name: str) -> dict[str, int] | None:
+    def create_criticity(self, name: str) -> dict[str, int]:
         query: str = """INSERT INTO criticities (name) VALUES (?);"""
         data = self._execute_query(query, (name,))
-        if data["rowcount"] == 1:
-            return {"id": data["lastrowid"]}
+        return {"id": data["lastrowid"]}
 
     def get_criticities(self) -> list[dict[str, Any]]:
         query: str = """SELECT * FROM criticities;"""
         return self._execute_query(query, mode=ExecuteMode.ALL)
+
+    def get_criticity_by_id(self, id: int) -> dict[str, str | int] | None:
+        query: str = """
+                    SELECT * FROM criticities WHERE id = ?;
+                    """
+        return self._execute_query(query, (id,), mode=ExecuteMode.ONE)
 
     def update_criticity(self, id: int, new_name: str) -> dict[str, int | None]:
         query: str = """
@@ -20,6 +25,6 @@ class CriticityRepository(BaseRepository):
                     """
         return self._execute_query(query, (new_name, id))
 
-    def delete_criticity(self, id: int) -> dict[str, int | None]:
+    def delete_criticity(self, id: int) -> dict[str, int]:
         query: str = """DELETE FROM criticities WHERE id = ?;"""
         return self._execute_query(query, (id,))
