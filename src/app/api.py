895ebe009a916e74dfd7a30381535966ed49b27c
Carlos <rocarlos1205@gmail.com>
2026-04-12 22:11:19 -0500

    chore: add servers property to app in order to improve Swagger documentation
    This changes base url for requests in SwaggerUI

--- src/app/api.py
+++ src/app/api.py
@@ -7,7 +7,13 @@ from app.routers import tasks, criticities, tags
 app = FastAPI(
     title="Tasks API",
     summary="API REST para gestión de tareas.",
-    description="Acopla todos los endpoints habilitados para la gestión de tareas."
+    description="Acopla todos los endpoints habilitados para la gestión de tareas.",
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Local"
+        }
+    ]
 )
 
 @app.get(
